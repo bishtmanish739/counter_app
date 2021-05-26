90a2447b393d7ce192b2d5c4bb99572352c7b907
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2021-05-26 12:07:53 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:counter_app/counter_bloc.dart';
+import 'package:counter_app/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,21 +11,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Counter App'),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -86,15 +82,12 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'You have pushed the button this many times:',
             ),
-            StreamBuilder(
-                stream: counterBloc.conterStream,
-                initialData: 0,
-                builder: (context, snapshot) {
-                  return Text(
-                    '${snapshot.data}',
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                }),
+            BlocBuilder<CounterCubit, CounterState>(builder: (context, state) {
+              return Text(
+                '${state.counterValue}',
+                style: Theme.of(context).textTheme.headline4,
+              );
+            }),
           ],
         ),
       ),
@@ -103,14 +96,16 @@ class _MyHomePageState extends State<MyHomePage> {
         children: [
           FloatingActionButton(
             onPressed: () {
-              counterBloc.eventSink.add(CounterAction.Inrement);
+              BlocProvider.of<CounterCubit>(context).increment();
+              //counterBloc.eventSink.add(CounterAction.Inrement);
             },
             tooltip: 'Increment',
             child: Icon(Icons.add),
           ),
           FloatingActionButton(
             onPressed: () {
-              counterBloc.eventSink.add(CounterAction.Decrement);
+              BlocProvider.of<CounterCubit>(context).increment();
+              //counterBloc.eventSink.add(CounterAction.Decrement);
             },
             tooltip: 'Decrement',
             child: Icon(Icons.exposure_minus_1),
